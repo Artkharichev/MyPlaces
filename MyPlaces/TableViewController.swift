dadc0e55a6955301d0853c551772b9eaad2de6f9
Артём Харичев <>
2020-05-04 21:02:51 +0300

    App icon launchScreen Row hight

--- MyPlaces/TableViewController.swift
+++ MyPlaces/TableViewController.swift
@@ -29,11 +29,17 @@ class TableViewController: UITableViewController {
 
         cell.textLabel?.text = placesNames[indexPath.row]
         cell.imageView?.image = UIImage(named: placesNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
 
         return cell
     }
     
-
+    // MARK: - Table view delegate
+     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
    
     /*
     // MARK: - Navigation
