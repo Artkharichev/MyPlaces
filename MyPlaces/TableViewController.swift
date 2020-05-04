3ac5082bb600a38184b60a81a76bc1f7658dbb39
Артём Харичев <>
2020-05-04 19:39:06 +0300

    init

---
+++ MyPlaces/TableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TableViewController.swift
+//  MyPlaces
+//
+//  Created by Артём Харичев on 04.05.2020.
+//  Copyright © 2020 Artem Kharichev. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let placesNames = ["Dead sea", "Masada", "Kesaria", "Petra" , "Old City" ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placesNames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = placesNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: placesNames[indexPath.row])
+
+        return cell
+    }
+    
+
+   
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
