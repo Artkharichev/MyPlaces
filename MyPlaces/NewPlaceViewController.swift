f68915c874d8e03633a583e8ef28f0692335c022
Артём Харичев <>
2020-05-13 21:42:07 +0300

    add stars rating

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -25,7 +25,10 @@ class NewPlaceViewController: UITableViewController {
         
         
         
-        tableView.tableFooterView = UIView()
+        tableView.tableFooterView = UIView(frame: CGRect(x: 0,
+                                                         y: 0,
+                                                         width: tableView.frame.size.width,
+                                                         height: 1.0))
         saveButton.isEnabled = false
         placeName.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
         setupEditScreen()
